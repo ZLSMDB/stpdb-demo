9e4c404a39f7778a87f280ef6dfe1809e3b1ad30
root <root@DESKTOP-Q1V71KG>
2024-09-11 18:38:59 +0800

    add minio storage

--- main.go
+++ main.go
@@ -1,49 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io"
 	"log"
+	"os"
+	"time"
 	"path/filepath"
+
 	db "github.com/ZLSMDB/stpdb-demo/src"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
 )
-
-// func main() {
-// 	// 初始化数据库
-// 	ldb, err := db.NewLevelDB("test.db")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	defer ldb.Close()
-
-// 	for i := 0; i < 1000; i++ {
-// 		key := []byte("prefix_" + fmt.Sprint(i))
-// 		value := []byte("value" + string(i))
-// 		err := ldb.Put(key, value)
-// 		if err != nil {
-// 			log.Fatal(err)
-// 		}
-// 	}
-
-// 	// 导出前缀为 "prefix_" 的所有键值对到文件中
-// 	err = ldb.ExportPrefixToFile([]byte("prefix_"))
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	// 导入文件中的键值对到数据库
-// 	// err = ldb.ImportFromFile("prefix_.stp")
-// 	// if err != nil {
-// 	// 	log.Fatal(err)
-// 	// }
-// 	// log.Println("Imported key-value pairs from file.")
-
-// 	// 修改文件中的某个键值对
-// 	err = ldb.ModifyValueInFile("prefix_.stp", "prefix_1", "new_value2")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	log.Println("Modified value in file.")
-// }
 
 func main() {
 	// 初始化 LevelDB
@@ -53,29 +22,121 @@ func main() {
 	}
 	defer ldb.Close()
 
-	stepFilePath := "/home/step192/step_data/5604121-C01_20171023.stp" // 你的 STEP 文件路径
+	// STEP 文件路径
+	stepFilePath := "/home/step192/step_data/1000410-28L.stp"
+
 	// 提取 STEP 文件名（不含路径和扩展名）
 	stepFileName := filepath.Base(stepFilePath)
 	stepFileName = stepFileName[:len(stepFileName)-len(filepath.Ext(stepFileName))]
 
 	// 解析 STEP 文件
-	parsedData, err := ldb.ParseStepFile(stepFilePath)
+	keys, values, err := ldb.ParseStepFile(stepFilePath)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("STEP file parsed successfully.")
+	fmt.Println("step文件解析完成.")
 
 	// 将解析后的数据存储到 LevelDB
-	err = ldb.StoreParsedData(parsedData, stepFileName)
+	err = ldb.StoreParsedData(stepFileName, keys, values)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Parsed data stored in LevelDB successfully.")
+	fmt.Println("step文件对应的kv存储完成.")
 
-	// 导出具有特定前缀的数据到文件
-	prefix := "5604121-C01_20171023"
-	err = ldb.ExportPrefixToFile([]byte(prefix))
+	// // 导出具有特定前缀的数据到文件
+	// prefix := "5604121-C01_20171023"
+	// err = ldb.ExportPrefixToFile([]byte(prefix))
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// 设置 MinIO 客户端参数
+	endpoint := "10.176.34.130:9006"
+	accessKeyID := "admin"
+	secretAccessKey := "admin123456"
+	useSSL := false
+
+	// 初始化 MinIO 客户端
+	minioClient, err := minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+		Secure: useSSL,
+	})
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("MinIO 客户端初始化失败:", err)
 	}
+
+	prefix := "1000410-28L"
+	// MinIO 存储桶和对象名称
+	bucketName := "1000410-28l" // 存储桶名称好像需要符合规则
+	ctx := context.Background()
+	exists, err := minioClient.BucketExists(ctx, bucketName)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// 创建bucket
+	if !exists {
+		err = minioClient.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{})
+		if err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Printf("存储桶%s已创建\n", bucketName)
+	} else {
+		fmt.Printf("存储桶%s已存在\n", bucketName)
+	}
+
+	// 获取存储桶中现有的对象
+	objectCh := minioClient.ListObjects(ctx, bucketName, minio.ListObjectsOptions{Recursive: true})
+	var fileCount int
+
+	for obj := range objectCh {
+		if obj.Err != nil {
+			log.Fatalln(obj.Err)
+		}
+		fileCount++
+	}
+	// 生成新的对象名称
+	objectName := fmt.Sprintf("%s_v%d.stp", prefix, fileCount+1)
+
+	// 调用导出函数
+	err = ldb.ExportPrefixToMinIO([]byte(prefix), minioClient, bucketName, objectName)
+	if err != nil {
+		fmt.Println("导出失败:", err)
+	} else {
+		fmt.Println("导出成功")
+	}
+
+	// 从minio中下载stp文件到本地
+	localFilePath := "/home/step192/step_data/" + objectName // 本地保存路径
+	ctx = context.Background()
+	err = downloadFileFromMinIO(ctx, minioClient, bucketName, objectName, localFilePath)
+	if err != nil {
+		fmt.Println("下载失败:", err)
+	} else {
+		fmt.Println("下载成功")
+	}
+}
+
+// 从 MinIO 下载文件并保存到本地
+func downloadFileFromMinIO(ctx context.Context, minioClient *minio.Client, bucketName, objectName, localFilePath string) error {
+	start := time.Now()
+	object, err := minioClient.GetObject(ctx, bucketName, objectName, minio.GetObjectOptions{})
+	if err != nil {
+		return fmt.Errorf("从 MinIO 获取对象失败: %v", err)
+	}
+	defer object.Close()
+
+	file, err := os.Create(localFilePath)
+	if err != nil {
+		return fmt.Errorf("创建本地文件失败: %v", err)
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, object)
+	if err != nil {
+		return fmt.Errorf("将文件内容复制到本地文件失败: %v", err)
+	}
+	elapsed := time.Since(start)
+	fmt.Printf("文件%s从MinIO下载并保存到本地路径%s, 用时 %.2f 秒\n", objectName, localFilePath, elapsed.Seconds())
+	return nil
 }
--- src/db.go
+++ src/db.go
@@ -8,7 +8,9 @@ import (
 	"strings"
 	"sync"
 	"time"
+	"context"
 
+	"github.com/minio/minio-go/v7"
 	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/syndtr/goleveldb/leveldb/util"
 )
@@ -65,7 +67,7 @@ func (ldb *LevelDB) Delete(key []byte) error {
 	return ldb.db.Delete(key, nil)
 }
 
-// ExportPrefixToFile exports all key-value pairs with the same prefix to a file.
+// ExportPrefixToFile 将所有具有相同前缀的键值对导出到文件中。
 func (ldb *LevelDB) ExportPrefixToFile(prefix []byte) error {
 	ldb.lock.RLock()
 	defer ldb.lock.RUnlock()
@@ -75,7 +77,46 @@ func (ldb *LevelDB) ExportPrefixToFile(prefix []byte) error {
 	filename := fmt.Sprintf("%s.stp", prefix)
 	file, err := os.Create(filename)
 	if err != nil {
-		return err
+		return fmt.Errorf("创建文件失败: %w", err)
+	}
+	defer file.Close()
+	writer := bufio.NewWriter(file)
+	defer writer.Flush()
+	iter := ldb.db.NewIterator(util.BytesPrefix(prefix), nil)
+	defer iter.Release()
+
+	for iter.Next() {
+		key := iter.Key()
+		value := iter.Value()
+		if len(key) <= len(prefix)+1 {
+			continue
+		}
+		line := fmt.Sprintf("#%s=%s;\n", key[len(prefix)+1:], value)
+		if _, err := writer.WriteString(line); err != nil {
+			return fmt.Errorf("写入文件失败: %w", err)
+		}
+	}
+
+	if err := iter.Error(); err != nil {
+		return fmt.Errorf("迭代器错误: %w", err)
+	}
+
+	elapsed := time.Since(start)
+	fmt.Printf("前缀为 '%s' 的数据成功导出到 %s，用时 %.2f 秒。\n", prefix, filename, elapsed.Seconds())
+	return nil
+}
+
+func (ldb *LevelDB) ExportPrefixToMinIO(prefix []byte, minioClient *minio.Client, bucketName, objectName string) error {
+	ldb.lock.RLock()
+	defer ldb.lock.RUnlock()
+
+	start := time.Now()
+
+	// 创建本地临时文件
+	filename := fmt.Sprintf("%s.stp", prefix)
+	file, err := os.Create(filename)
+	if err != nil {
+		return fmt.Errorf("创建文件失败: %w", err)
 	}
 	defer file.Close()
 
@@ -85,21 +126,33 @@ func (ldb *LevelDB) ExportPrefixToFile(prefix []byte) error {
 	for iter.Next() {
 		key := iter.Key()
 		value := iter.Value()
+
+		if len(key) <= len(prefix)+1 {
+			continue
+		}
 		line := fmt.Sprintf("#%s=%s;\n", key[len(prefix)+1:], value)
-		// line := fmt.Sprintf("%s:%s\n", key, value)
 		if _, err := file.WriteString(line); err != nil {
-			return err
+			return fmt.Errorf("写入文件失败: %w", err)
 		}
 	}
 
 	if err := iter.Error(); err != nil {
-		return err
+		return fmt.Errorf("迭代器错误: %w", err)
 	}
+
+	// 将文件上传到 MinIO
+	ctx := context.Background()
+	_, err = minioClient.FPutObject(ctx, bucketName, objectName, filename, minio.PutObjectOptions{})
+	if err != nil {
+		return fmt.Errorf("文件上传到 MinIO 失败: %w", err)
+	}
+
 	// 计算导出时间
 	elapsed := time.Since(start)
-	fmt.Printf("Data with prefix '%s' exported to %s successfully in %v seconds.\n", prefix, filename, elapsed.Seconds())
+	fmt.Printf("前缀为'%s'的数据成功导出到MinIO中'%s/%s'，用时 %.2f 秒。\n", prefix, bucketName, objectName, elapsed.Seconds())
 	return nil
 }
+
 
 // // ImportFromFile loads key-value pairs from a file into the LevelDB.
 // func (ldb *LevelDB) ImportFromFile(filename string) error {
@@ -125,58 +178,81 @@ func (ldb *LevelDB) ExportPrefixToFile(prefix []byte) error {
 // }
 
 // ParseStepFile 解析 STEP 文件，返回所有的键值对
-func (ldb *LevelDB) ParseStepFile(filepath string) (map[string]string, error) {
+func (ldb *LevelDB) ParseStepFile(filepath string) ([]string, [][]byte, error) {
 	// 记录解析开始时间
 	start := time.Now()
+
+	var keys []string
+	var values [][]byte
+
 	file, err := os.Open(filepath)
 	if err != nil {
-		return nil, err
+		return nil, nil,  err
 	}
 	defer file.Close()
-
-	// 存储解析出的键值对
-	parsedData := make(map[string]string)
 	// 正则表达式匹配 STEP 文件行
 	re := regexp.MustCompile(`#(\d+)=(.+);`)
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
+	// 使用 bufio.Reader 替代 bufio.Scanner
+	reader := bufio.NewReader(file)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err.Error() == "EOF" {
+				break // 文件读取完毕
+			}
+			return nil, nil, err
+		}
+
+		// 处理每一行
+		line = line[:len(line)-1] // 去掉行末的换行符
 		matches := re.FindStringSubmatch(line)
 		if len(matches) != 3 {
 			continue // 跳过不匹配的行
 		}
 		key := matches[1]
 		value := matches[2]
-		parsedData[key] = value
+		keys = append(keys, key)
+		values = append(values, []byte(value))
 	}
 
-	if err := scanner.Err(); err != nil {
-		return nil, err
-	}
 	// 记录解析结束时间并计算耗时
 	elapsed := time.Since(start)
-	fmt.Printf("Time taken to parse step data to kv pairs: %v seconds\n", elapsed.Seconds())
-	return parsedData, nil
+	fmt.Printf("将step文件解析成kv对的时间: %v seconds\n", elapsed.Seconds())
+	return keys, values, nil
 }
 
 // StoreParsedData 存储解析出的键值对到 LevelDB 中
-func (ldb *LevelDB) StoreParsedData(data map[string]string, stepFileName string) error {
+func (ldb *LevelDB) StoreParsedData(stepFileName string, keys []string, values [][]byte) error {
 	// 记录存储开始时间
 	start := time.Now()
 
-	for key, value := range data {
+	const batchSize = 1000 // 设置适当的批量大小
+	batch := new(leveldb.Batch)
+
+	for i := range keys {
 		// 为 key 添加文件名前缀
-		fullKey := stepFileName + "_" + key
-		err := ldb.Put([]byte(fullKey), []byte(value))
-		if err != nil {
+		fullKey := stepFileName + "_" + keys[i]
+		batch.Put([]byte(fullKey), values[i])
+		if (i+1)%batchSize == 0 {
+			if err := ldb.db.Write(batch, nil); err != nil {
+				return err
+			}
+			// 清空 batch
+			batch = new(leveldb.Batch)
+		}
+	}
+
+	// 写入剩余的操作
+	if batch.Len() > 0 {
+		if err := ldb.db.Write(batch, nil); err != nil {
 			return err
 		}
 	}
 
 	// 记录存储结束时间并计算耗时
 	elapsed := time.Since(start)
-	fmt.Printf("Time taken to store data in LevelDB: %v seconds\n", elapsed.Seconds())
+	fmt.Printf("将解析后的 kv 对存进 LevelDB 的时间: %v seconds\n", elapsed.Seconds())
 	return nil
 }
 
